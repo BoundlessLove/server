4747b154e520a496c5b9a4090515248d1979736a
Jay Sarna <general@systematicdefence.tech>
2026-08-12 08:35:07 +0000

    updates

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,5 @@
 import sys
 import os
-import asyncio
 
 # 1. Force Python to look in your current folder for code 
 sys.path.insert(0, os.path.dirname(__file__)) 
@@ -9,17 +8,10 @@ sys.path.insert(0, os.path.dirname(__file__))
 venv_packages = '/home/systema1/virtualenv/privateapps.systematicdefence.tech/server/3.10/lib/python3.10/site-packages'
 sys.path.insert(0, venv_packages)
 
-# 3. Secure a stable asyncio event loop for this worker process thread
-try:
-    loop = asyncio.get_event_loop()
-except RuntimeError:
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-
-# 4. Import your FastAPI app object from main.py 
+# 3. Import your FastAPI app object from main.py 
 from main import app 
 
-# 5. Convert ASGI to WSGI using ASGIMiddleware from a2wsgi
+# 4. Convert ASGI to WSGI using ASGIMiddleware from a2wsgi
 from a2wsgi import ASGIMiddleware
 
 application = ASGIMiddleware(app)
