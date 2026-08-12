a5aa5f25fbd04e7fde78bf82c2eeed6fa9ad11a5
Jay Sarna <general@systematicdefence.tech>
2026-08-12 10:07:08 +0000

    logs

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,12 @@
-import imp
-import os
 import sys
+import os
 
+# 1. Force Python to look in your current folder for code 
+sys.path.insert(0, os.path.dirname(__file__)) 
 
-sys.path.insert(0, os.path.dirname(__file__))
+# 2. Force the server to use your virtual environment packages
+venv_packages = '/home/systema1/virtualenv/privateapps.systematicdefence.tech/server/3.10/lib/python3.10/site-packages'
+sys.path.insert(0, venv_packages)
 
-wsgi = imp.load_source('wsgi', 'passenger_wsgi.py')
-application = wsgi.application
+# 3. Direct bridge to your Flask application inside main.py
+from main import application
